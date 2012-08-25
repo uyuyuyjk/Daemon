50b99ec10410f91d138ad2404a16fca8109fe9cf
tcooc <tco_oc1111@hotmail.com>
2012-08-25 17:17:03 -0500

    Readme

--- src/common/tco/daemon/ProxyClient.java
+++ src/common/tco/daemon/ProxyClient.java
@@ -9,8 +9,9 @@ import net.minecraftforge.client.MinecraftForgeClient;
 public class ProxyClient extends ProxyCommon {
 	@Override
 	public void registerRenderInformation() {
-		MinecraftForgeClient.preloadTexture("tco/daemon/sprites/blocks.png");
-		MinecraftForgeClient.preloadTexture("tco/daemon/sprites/feeder.png");
-		MinecraftForgeClient.preloadTexture("tco/daemon/sprites/matrix.png");
+		MinecraftForgeClient.preloadTexture("/tco/daemon/sprites/daemonitems.png");
+		MinecraftForgeClient.preloadTexture("/tco/daemon/sprites/blocks.png");
+		MinecraftForgeClient.preloadTexture("/tco/daemon/sprites/feeder.png");
+		MinecraftForgeClient.preloadTexture("/tco/daemon/sprites/matrix.png");
 	}
 }
